03b6db4ced880c8becdcda49bfc8cb2e7916f159
Vakurin <vakurin.maxim@gmail.com>
2022-08-30 03:51:45 +0300

    added gas report

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-web3";
+import "solidity-coverage";
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import "@nomicfoundation/hardhat-chai-matchers";
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
@@ -8,6 +14,13 @@ const config: HardhatUserConfig = {
     hardhat: {
       allowUnlimitedContractSize: true
     }
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
 
