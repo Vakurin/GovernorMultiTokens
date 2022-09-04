a2ea1598a552767757490080a769d5bf800c8d59
Vakurin <vakurin.maxim@gmail.com>
2022-09-04 19:39:26 +0300

    del

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 // import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
-// import "@nomiclabs/hardhat-web3";
 import "solidity-coverage";
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
-// import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
